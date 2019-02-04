d7a80882f08c23598209affdeac291a20702ec68
Devendranath <dnreddy890@gmail.com>
2019-02-04 10:16:57 +0530

    Base app is created,

--- TableViewTest-1/ViewController.swift
+++ TableViewTest-1/ViewController.swift
@@ -26,7 +26,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+    } 
     
     override func viewWillAppear(_ animated: Bool) {
         tableView.reloadData()
